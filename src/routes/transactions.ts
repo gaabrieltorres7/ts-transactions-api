88a3449ac372be3c9d66aa382f4dd6459286d340
Gabriel Torres <gaabrieltt7@gmail.com>
2023-04-26 21:24:10 -0300

    feat: create transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,24 @@
 import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
 import { db } from '../db'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await db('transactions')
-      .where('amount', 1000)
-      .select('*')
+  app.post('/create', async (req, res) => {
+    const createTransactionSchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const { title, amount, type } = createTransactionSchema.parse(req.body)
+
+    await db('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : -amount,
+    })
+
+    return res.status(201).send({ message: 'Transaction created succesfuly' })
   })
 }
